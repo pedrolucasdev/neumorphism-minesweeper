797ecbfa879e870ad19791f80f1cca9087b9cdd8
pedrolucasdev <pedrolucasdev@outlook.pt>
2020-08-04 13:12:40 +0100

    Updates

---
+++ precache-manifest.65bd35f702dfb30bc428c8f1835f74e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "94fb6129646227e74bcb",
+    "url": "/neumorphism-minesweeper/static/js/main.d8880588.chunk.js"
+  },
+  {
+    "revision": "71e58402bb0ca7cb4289",
+    "url": "/neumorphism-minesweeper/static/js/runtime~main.f8659d1c.js"
+  },
+  {
+    "revision": "576404e2b732abc3a8f9",
+    "url": "/neumorphism-minesweeper/static/js/2.41ed8ee8.chunk.js"
+  },
+  {
+    "revision": "a3d5e570e7d0d63bdbe9e48e852fb994",
+    "url": "/neumorphism-minesweeper/index.html"
+  }
+];
